62901f2004c8dd5fe56e8dd05cc5886ce3a01811
robin* <robin_maki@planet.moe>
2024-10-16 11:48:51 +0900

    모듈화

--- src/routes/nav/Navbar.svelte
+++ src/routes/nav/Navbar.svelte
@@ -2,9 +2,10 @@
 	import { getContext } from 'svelte';
 	import type { Writable } from 'svelte/store';
 	import { createDropdownMenu, melt } from '@melt-ui/svelte';
+	import NavItem from './NavItem.svelte';
 	import type { AccountSession } from '$lib/types/session';
 
-	const user = getContext<Writable<AccountSession>>('user');
+	const user = getContext<Writable<AccountSession | undefined>>('user');
 
 	const {
 		elements: { menu, item, trigger },
@@ -12,13 +13,11 @@
 </script>
 
 <nav class="flex bb:1|gray-10 bg:white justify-content:center w:100%">
-	<div
-		class="flex align-items:center background-color:gray-10>a:hover background-color:gray-10>button:hover gap:24 h:48 max-w:1200 p:16>a p:16>button w:100%"
-	>
-		<a class="font-weight:bold" href="/">Quasar</a>
+	<div class="flex align-items:center gap:24 h:48 max-w:1200 w:100%">
+		<NavItem class="font-weight:bold" href="/" tag="a">Quasar</NavItem>
 		<div class="flex:1"></div>
 		{#if $user}
-			<button use:melt={$trigger}>{$user.name}</button>
+			<NavItem melt={$trigger} tag="button">{$user.name}</NavItem>
 			<div use:melt={$menu}>
 				<div use:melt={$item}>
 					<!-- svelte-ignore a11y-invalid-attribute -->
