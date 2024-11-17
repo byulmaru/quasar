2715d63ddd7509fc88fb1ee6495dda60fe320685
Jiyu Park <robin_maki@planet.moe>
2024-11-18 02:18:33 +0900

    유저 페이지 백엔드 구현 (#14)

--- src/lib/database/schema.ts
+++ src/lib/database/schema.ts
@@ -1,5 +1,11 @@
-import { json, pgEnum, pgTable, varchar } from 'drizzle-orm/pg-core';
-import { sql } from 'drizzle-orm';
+import {
+	json,
+	pgEnum,
+	pgTable,
+	uniqueIndex,
+	varchar,
+} from 'drizzle-orm/pg-core';
+import { eq, sql } from 'drizzle-orm';
 import cryptoRandomString from 'crypto-random-string';
 import { datetime, id, idPk } from './types';
 
@@ -8,11 +14,24 @@ type OAuthAppInfo = {
 	clientSecret: string;
 };
 
-export const Accounts = pgTable('accounts', {
-	id: idPk(),
-	name: varchar('name').notNull(),
-	avatarUrl: varchar('avatar_url'),
-});
+export const AccountState = pgEnum('AccountState', [
+	'ACTIVE',
+	'DELETED',
+	'SUSPENDED',
+]);
+export const Accounts = pgTable(
+	'accounts',
+	{
+		id: idPk(),
+		state: AccountState('state').notNull().default('ACTIVE'),
+		slug: varchar('slug').notNull(),
+		name: varchar('name').notNull(),
+		avatarUrl: varchar('avatar_url'),
+	},
+	(t) => ({
+		slugUniqueIndex: uniqueIndex().on(t.slug).where(eq(t.state, 'ACTIVE')),
+	}),
+);
 
 export const AccountOAuthApps = pgTable('account_oauth_apps', {
 	id: idPk(),
---
+++ src/routes/(default)/profile/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let data;
+</script>
+
+<div>
+	<h1>{data.account.name}</h1>
+	<img
+		alt={`${data.account.name} 의 프로필 이미지`}
+		src={data.account.avatarUrl}
+	/>
+</div>
