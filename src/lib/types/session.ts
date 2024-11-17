1cda8e8867d57e55f7457cd785623a3af285f82c
robin* <robin_maki@planet.moe>
2024-11-18 02:16:20 +0900

    헤더에 프로필 링크 추가

--- src/lib/types/session.ts
+++ src/lib/types/session.ts
@@ -1,4 +1,5 @@
 export type AccountSession = {
 	name: string;
+	slug: string;
 	avatarUrl: string;
 };
--- src/routes/nav/Navbar.svelte
+++ src/routes/nav/Navbar.svelte
@@ -18,15 +18,26 @@
 		</a>
 		<div class="flex:1"></div>
 		{#if $user}
-			<button class="min-width:140 background-color:gray-10:hover p:16" use:melt={$trigger}>
+			<button
+				class="background-color:gray-10:hover min-width:140 p:16"
+				use:melt={$trigger}
+			>
 				{$user.name}
 			</button>
-			<div use:melt={$menu} class="width:140 bg:white top:3rem! display:flex flex-direction:column align-items:center border-radius:0|0|5|5 box-shadow:2|2|3|#c7c7c722">
-				<div use:melt={$item} class="padding:10 width:140 text-align:center bg:rgb(239|238|240):hover">
-					<!-- svelte-ignore a11y-invalid-attribute -->
-					<a href="#">프로필</a>
+			<div
+				class="align-items:center bg:white border-radius:0|0|5|5 box-shadow:2|2|3|#c7c7c722 display:flex flex-direction:column top:3rem! width:140"
+				use:melt={$menu}
+			>
+				<div
+					class="bg:rgb(239|238|240):hover padding:10 text-align:center width:140"
+					use:melt={$item}
+				>
+					<a href={`/profile/${$user.slug}`}>프로필</a>
 				</div>
-				<div use:melt={$item} class="padding:10 width:140 text-align:center bg:rgb(239|238|240):hover">
+				<div
+					class="bg:rgb(239|238|240):hover padding:10 text-align:center width:140"
+					use:melt={$item}
+				>
 					<form action="/logout" method="post">
 						<button type="submit">로그아웃</button>
 					</form>
