9049981d625a9a90f75044fa88c0f04d9066b0af
jyhyun1008 <jayhyun1008@gmail.com>
2024-11-18 11:08:50 +0900

    유저 페이지 UI

--- src/routes/(default)/profile/[slug]/+page.svelte
+++ src/routes/(default)/profile/[slug]/+page.svelte
@@ -1,11 +1,20 @@
 <script lang="ts">
+	import { resolveRoute } from '$app/paths';
+	import { page } from '$app/stores';
 	export let data;
 </script>
 
 <div>
-	<h1>{data.account.name}</h1>
-	<img
+	<h1 class="margin-bottom:1rem">{data.account.name} 님의 프로필</h1>
+	<div class="width:100% padding:10 display:flex gap:10 background:white border-radius:5">
+		<img
 		alt={`${data.account.name} 의 프로필 이미지`}
 		src={data.account.avatarUrl}
-	/>
+		class="max-width:256px"
+		/>
+		<div>
+			<h1>{data.account.name}</h1>
+			<div>{$page.params.slug}</div>
+		</div>
+	</div>
 </div>
