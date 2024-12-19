3b48726d0ac5b0e964b8503b4e68d10c446643e0
Hyun Jaeyeon <jayhyun1008@gmail.com>
2024-12-19 15:26:48 +0900

    박스 설정 페이지 (#19)

--- src/routes/(default)/box/[slug]/+page.svelte
+++ src/routes/(default)/box/[slug]/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
+    import { page } from '$app/stores';
 	import { superForm } from 'sveltekit-superforms';
 	import TextArea from '$lib/components/TextArea.svelte';
 
 	const { data } = $props();
 
 	const superform = superForm(data.form);
+
+	//TODO: 자기 자신일 때만
+	const myBox = true
 </script>
 
 <div
@@ -13,6 +17,9 @@
 	<div>
 		<h1>{data.box.name}</h1>
 		<div>{data.box.description}</div>
+		{#if myBox}
+		<div><a href={`${$page.params.slug}/manage`}>질문상자 관리</a></div>
+		{/if}
 	</div>
 
 	<form method="POST" use:superform.enhance>
---
+++ src/routes/(default)/box/[slug]/manage/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { superForm } from 'sveltekit-superforms';
+	import Button from '$lib/components/Button.svelte';
+	import Select from '$lib/components/Select.svelte'; //TODO
+	import InputField from '$lib/components/InputField.svelte';
+
+	const { data } = $props();
+	const superform = superForm(data.form);
+	let { form } = superform;
+
+    const allowAnonymOptions = {
+        texts: ['허용합니다', '허용하지 않습니다'],
+        values: ['true', 'false']
+    }
+
+    let slugUrl = $derived(`/box/${$form.slug}`)
+</script>
+
+<div
+	class="flex background:white border-radius:5 flex-direction:column gap:10 padding:10 width:100%"
+>
+    <form method="POST" use:superform.enhance class="flex flex-direction:column">
+        <InputField name="name" label="질문상자 이름" {superform}/>
+        <InputField id="slugurl" name="slug" label="질문상자 주소" description={slugUrl} {superform}/>
+        <Select name="allowanonym" label="익명 질문을" options={allowAnonymOptions} {superform}/>
+		<Button class="bg:#6364FF bg:#563ACC:hover color:white flex:1" theme="none" type="submit">수정</Button>
+    </form>
+
+</div>
