10220de752f14ea8688fc07c057f43a5d77070f1
Jiyu Park <robin_maki@planet.moe>
2024-12-19 18:07:37 +0900

    select 컴포넌트 업데이트 (#20)

--- src/lib/components/Select.svelte
+++ src/lib/components/Select.svelte
@@ -1,20 +1,21 @@
 <script lang="ts">
-	import type { HTMLInputAttributes } from 'svelte/elements';
+	import type { HTMLSelectAttributes } from 'svelte/elements';
 	import type { SuperForm } from 'sveltekit-superforms';
 
 	type Props = {
 		name: string;
-		options: Array<{
+		options: {
 			text: string;
 			value: string;
-		}>;
+		}[];
 		label?: string;
 		description?: string;
 		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		superform?: SuperForm<any>;
-	} & HTMLInputAttributes;
+	} & HTMLSelectAttributes;
 
-	let { name, options, label, description, superform, ...restProps }: Props = $props();
+	let { name, options, label, description, superform, ...restProps }: Props =
+		$props();
 	const { form, errors } = superform ?? {};
 </script>
 
@@ -27,10 +28,10 @@
 		aria-invalid={$errors?.[name] ? 'true' : undefined}
 		bind:value={$form[name]}
 	>
-        {#each options.texts as option, i}
-        <option value={options.values[i]}>{option}</option>
-        {/each}
-    </select>
+		{#each options as option}
+			<option value={option.value}>{option.text}</option>
+		{/each}
+	</select>
 	{#if $errors?.[name]}
 		<p class="color:red font:.75em">{$errors[name]}</p>
 	{/if}
--- src/routes/(default)/box/[slug]/manage/+page.svelte
+++ src/routes/(default)/box/[slug]/manage/+page.svelte
@@ -1,29 +1,45 @@
 <script lang="ts">
 	import { superForm } from 'sveltekit-superforms';
 	import Button from '$lib/components/Button.svelte';
-	import Select from '$lib/components/Select.svelte'; //TODO
+	import Select from '$lib/components/Select.svelte';
 	import InputField from '$lib/components/InputField.svelte';
 
 	const { data } = $props();
 	const superform = superForm(data.form);
 	let { form } = superform;
 
-    const allowAnonymOptions = {
-        texts: ['허용합니다', '허용하지 않습니다'],
-        values: ['true', 'false']
-    }
+	const allowAnonymOptions = [
+		{ text: '허용합니다', value: 'true' },
+		{ text: '허용하지 않습니다', value: 'false' },
+	];
 
-    let slugUrl = $derived(`/box/${$form.slug}`)
+	let slugUrl = $derived(`/box/${$form.slug}`);
 </script>
 
 <div
 	class="flex background:white border-radius:5 flex-direction:column gap:10 padding:10 width:100%"
 >
-    <form method="POST" use:superform.enhance class="flex flex-direction:column">
-        <InputField name="name" label="질문상자 이름" {superform}/>
-        <InputField id="slugurl" name="slug" label="질문상자 주소" description={slugUrl} {superform}/>
-        <Select name="allowanonym" label="익명 질문을" options={allowAnonymOptions} {superform}/>
-		<Button class="bg:#6364FF bg:#563ACC:hover color:white flex:1" theme="none" type="submit">수정</Button>
-    </form>
-
+	<form class="flex flex-direction:column" method="POST" use:superform.enhance>
+		<InputField name="name" label="질문상자 이름" {superform} />
+		<InputField
+			id="slugurl"
+			name="slug"
+			description={slugUrl}
+			label="질문상자 주소"
+			{superform}
+		/>
+		<Select
+			name="allowanonym"
+			label="익명 질문을"
+			options={allowAnonymOptions}
+			{superform}
+		/>
+		<Button
+			class="bg:#6364FF bg:#563ACC:hover color:white flex:1"
+			theme="none"
+			type="submit"
+		>
+			수정
+		</Button>
+	</form>
 </div>
